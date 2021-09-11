de19cf80e5ea3ec04937ea3f048e40be6d0a5a27
tamnguyen <tamnguyen02121993@gmail.com>
2021-09-11 12:15:12 +0700

    Setup routing using react-router-dom, @types/react-router-dom

---
+++ src/components/Common/PrivateRoute.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Redirect, Route, RouteProps } from 'react-router-dom';
+
+export function PrivateRoute(props: RouteProps) {
+  // Check if user is logged in
+  // If yes, show route
+  // Otherwise, redirect to login page
+
+  const isLogggedIn = Boolean(localStorage.getItem('access_token'));
+  if (!isLogggedIn) {
+    return <Redirect to="/login" />;
+  }
+
+  return <Route {...props} />;
+}
