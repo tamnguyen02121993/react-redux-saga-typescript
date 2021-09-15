0f67cabf74f47b7d0039c16f5461542c057aec01
tamnguyen <tamnguyen02121993@gmail.com>
2021-09-15 21:23:06 +0700

    Implement student list ui

---
+++ src/features/student/components/StudentTable.tsx
@@ -0,0 +1,63 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import { Student } from 'models';
+import React from 'react';
+import { Paper } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  table: {},
+  edit: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+export interface StudentTableProps {
+  studentList: Student[];
+  onEdit?: (student: Student) => void;
+  onRemove?: (student: Student) => void;
+}
+export function StudentTable({ studentList, onEdit, onRemove }: StudentTableProps) {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} size="small" aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Mark</TableCell>
+            <TableCell>City</TableCell>
+            <TableCell align="center">Action</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {studentList.map((student) => (
+            <TableRow key={student.id}>
+              <TableCell>{student.id}</TableCell>
+              <TableCell>{student.name}</TableCell>
+              <TableCell>{student.gender}</TableCell>
+              <TableCell>{student.mark}</TableCell>
+              <TableCell>{student.city}</TableCell>
+              <TableCell align="right">
+                <Button variant="contained" color="primary" onClick={() => onEdit?.(student)}>
+                  Edit
+                </Button>
+                <Button variant="outlined" color="secondary" onClick={() => onRemove?.(student)}>
+                  Remove
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/features/student/pages/ListPage.tsx
+++ src/features/student/pages/ListPage.tsx
@@ -1,5 +1,44 @@
-import * as React from 'react';
+import { Box, Typography, Button, makeStyles } from '@material-ui/core';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import React, { useEffect } from 'react';
+import { StudentTable } from '../components/StudentTable';
+import { selectStudentList, studentActions } from '../studentSlice';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  titleContainer: {
+    display: 'flex',
+    flexFlow: 'row nowrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: theme.spacing(4),
+  },
+}));
 
 export default function ListPage() {
-  return <div>List Page</div>;
+  const dispatch = useAppDispatch();
+  const classes = useStyles();
+  const studentList = useAppSelector(selectStudentList);
+
+  useEffect(() => {
+    dispatch(
+      studentActions.fetchStudentList({
+        _page: 1,
+        _limit: 20,
+      })
+    );
+  }, [dispatch]);
+  return (
+    <Box className={classes.root}>
+      <Box className={classes.titleContainer}>
+        <Typography variant="h4">Students</Typography>
+        <Button variant="contained" color="primary">
+          Add new student
+        </Button>
+      </Box>
+      {/* Student Table */}
+      <StudentTable studentList={studentList} />
+      {/* Pagination */}
+    </Box>
+  );
 }
