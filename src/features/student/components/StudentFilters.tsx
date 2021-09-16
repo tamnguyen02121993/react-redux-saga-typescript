50b39ede1402ff75eb196c707b16274fe24cc8a1
tamnguyen <tamnguyen02121993@gmail.com>
2021-09-16 18:19:11 +0700

    Add sort student and clear all filter

--- src/features/student/components/StudentFilters.tsx
+++ src/features/student/components/StudentFilters.tsx
@@ -1,5 +1,5 @@
 import { City, ListParams } from 'models';
-import React, { ChangeEvent } from 'react';
+import React, { ChangeEvent, useRef } from 'react';
 import {
   Box,
   Grid,
@@ -8,6 +8,7 @@ import {
   OutlinedInput,
   MenuItem,
   Select,
+  Button,
 } from '@material-ui/core';
 import { Search } from '@material-ui/icons';
 
@@ -24,6 +25,7 @@ export default function StudentFilters({
   onChange,
   onSearchChange,
 }: StudentFiltersProps) {
+  const searchNameRef = useRef<HTMLInputElement>();
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (!onSearchChange) return;
     const newFilter: ListParams = {
@@ -44,6 +46,33 @@ export default function StudentFilters({
     onChange(newFilter);
   };
 
+  const handleSortChange = (e: ChangeEvent<{ name?: string; value: unknown }>) => {
+    if (!onChange) return;
+    const [sort, order] = (e.target.value as string).split('.');
+    const newFilter: ListParams = {
+      ...filter,
+      _sort: sort || undefined,
+      _order: (order as 'asc' | 'desc') || undefined,
+    };
+    onChange(newFilter);
+  };
+
+  const handleClearFilter = () => {
+    if (!onChange) return;
+    const newFilter: ListParams = {
+      ...filter,
+      _sort: undefined,
+      _order: undefined,
+      _page: 1,
+      city: undefined,
+      name_like: undefined,
+    };
+    onChange(newFilter);
+    if (searchNameRef.current) {
+      searchNameRef.current.value = '';
+    }
+  };
+
   return (
     <Box>
       <Grid container spacing={3}>
@@ -56,11 +85,12 @@ export default function StudentFilters({
               endAdornment={<Search />}
               labelWidth={60}
               onChange={handleSearchChange}
+              inputRef={searchNameRef}
             />
           </FormControl>
         </Grid>
 
-        <Grid item xs={12} md={6} lg={3}>
+        <Grid item xs={12} md={6} lg={2}>
           <FormControl variant="outlined" size="small" fullWidth>
             <InputLabel id="filterByCity">Filter by city</InputLabel>
             <Select
@@ -79,6 +109,32 @@ export default function StudentFilters({
               ))}
             </Select>
           </FormControl>
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={2}>
+          <FormControl variant="outlined" size="small" fullWidth>
+            <InputLabel id="sortBy">Sort</InputLabel>
+            <Select
+              labelId="sortBy"
+              value={filter._sort ? `${filter._sort}.${filter._order}` : ''}
+              onChange={handleSortChange}
+              label="Sort"
+            >
+              <MenuItem value="">
+                <em>No sort</em>
+              </MenuItem>
+              <MenuItem value="name.asc">Name ASC</MenuItem>
+              <MenuItem value="name.desc">Name DESC</MenuItem>
+              <MenuItem value="mark.asc">Mark ASC</MenuItem>
+              <MenuItem value="mark.desc">Mark DESC</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+
+        <Grid item xs={12} md={6} lg={2}>
+          <Button variant="outlined" color="primary" onClick={handleClearFilter}>
+            Clear Filter
+          </Button>
         </Grid>
       </Grid>
     </Box>
